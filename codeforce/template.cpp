b2b0856848b7f050f9f9407c7a757e5d71e652f2
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-02 20:04:36 +0530

    template changes

--- codeforce/template.cpp
+++ codeforce/template.cpp
@@ -1,9 +1,9 @@
 #pragma GCC optimize("O3,unroll-loops")
 #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
 #include <iostream>
 #include "bits/stdc++.h"
 #define fst ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define arrin for(int i=0; i<n; i++) cin>>arr[i];
 #define          all(v)              v.begin(), v.end()
 #define         rall(v)              v.rbegin(),v.rend()
 #define            emp                emplace_back
@@ -12,6 +12,8 @@
 #define no cout<<"NO\n";
 #define ll long long int
 #define vec vector<int>
+#define rep(a,b) for (int i = a; i < b; i++)
+
 const int MOD = 1e+7;
 
 using namespace std;
@@ -24,7 +26,6 @@ void solve(){
 
 int main(){
     fst;
-    cin.tie(NULL);
     int t = 1;
     cin>>t;
 
