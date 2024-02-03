a53648f100218dd625337ac3230b8e7dae55e5d7
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-03 19:40:53 +0530

    Restructured the template

--- codeforce/template.cpp
+++ codeforce/template.cpp
@@ -3,16 +3,18 @@
 
 #include <iostream>
 #include "bits/stdc++.h"
+
 #define fst ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 #define          all(v)              v.begin(), v.end()
 #define         rall(v)              v.rbegin(),v.rend()
 #define            emp                emplace_back
 #define          sz(a)               (int)a.size()
-#define yes cout<<"YES\n";
-#define no cout<<"NO\n";
-#define ll long long int
 #define vec vector<int>
 #define rep(a,b) for (int i = a; i < b; i++)
+#define ll long long int
+
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
 
 const int MOD = 1e+7;
 
