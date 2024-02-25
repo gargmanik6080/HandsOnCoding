840ceac4aad885e13d91f973af8bb6f6d79ade72
gargmanik6080 <gargmanik6080@gmail.com>
2024-02-25 16:09:16 +0530

    Refactor code to improve performance and readability

---
+++ codeforce/leetcode.cpp
@@ -0,0 +1,14 @@
+
+#define fst ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define          all(v)              v.begin(), v.end()
+#define         rall(v)              v.rbegin(),v.rend()
+#define            emp                emplace_back
+#define          sz(a)               (int)a.size()
+#define vec vector<int>
+#define rep(a,b) for (int i = a; i < b; i++)
+#define ll long long int
+
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+
+const int MOD = 1e+7;
