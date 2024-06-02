212a6d7f9e5a02077ae5c8c54ef9301cd686c372
gargmanik6080 <gargmanik6080@gmail.com>
2024-06-02 17:42:44 +0530

    MAC Setup

--- codeforce/Practise/1829D_Gold_Rush.cpp
+++ codeforce/Practise/1829D_Gold_Rush.cpp
@@ -1,5 +1,5 @@
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+// #pragma GCC optimize("O3,unroll-loops")
+// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
 #include<iostream>
 #include <bits/stdc++.h>
 #define fst ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
